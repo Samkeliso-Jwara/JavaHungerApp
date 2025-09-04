3eaae796ff4d462e5f3f1d3734b4ccd5bd8cfcd8
Sam <samjwara01@gmail.com>
2025-09-04 08:14:48 +0000

    First Commit

---
+++ src/hungerstatus/HungerStatus.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hungerstatus;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class HungerStatus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner hunger = new Scanner(System.in);
+        
+        System.out.println("Hi there what is your name?");
+        String name = hunger.nextLine();
+        
+        System.out.println("Hi " + name + " what is your favourite meal?");
+        String favMeal = hunger.nextLine();
+        
+        System.out.println("Hi " + name + " are you hungry? (Yes/No)");
+        String hungry = hunger.nextLine().toLowerCase();//Priming input  
+        
+        while (hungry.equals("yes")){
+            System.out.println("You can take another bite of "
+            + favMeal);
+            
+            System.out.println( name + " are you still hungry?");
+            
+            hungry = hunger.nextLine().toLowerCase();// Updating the priming input
+            
+            
+        }
+        while(!hungry.equals("yes") && !hungry.equals("no")){
+                System.out.println("Please select either YES or NO");
+                System.out.println("Hi " + name + " are you hungry? (Yes/No)");
+                hungry = hunger.nextLine().toLowerCase();
+            }
+        System.out.println("Great! You are now full");
+        hunger.close();
+    }
+    
+}
